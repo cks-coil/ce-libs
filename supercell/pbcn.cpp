be0da853014e43d018d48b030bc9ba4e915622da
Chikashi Shinagawa <cks.shina@gmail.com>
2015-07-29 18:17:40 +0900

    pbcn: write calcSymmetryMatrices

--- supercell/pbcn.cpp
+++ supercell/pbcn.cpp
@@ -20,4 +20,18 @@ void PbcnSupercell::calcUnitCellFractionalPositions(void){
     unitCellFractionalPositions.push_back( Vector3d(-x, -y, -z) );
 }
 
-void PbcnSupercell::calcSymmetryMatrices(void){}
+void PbcnSupercell::calcSymmetryMatrices(void){
+    int maxN = getCellSize().maxCoeff() * 2;
+    MatrixXi alphaBMatrix, betaCMatrix, gammaNMatrix;
+    alphaBMatrix = getSymmetryMatrix( fractionalPositions, glideReflection(fractionalPositions, Vector3d(0,0.5,0), Vector3d(0.25,0,0)) );
+    betaCMatrix = getSymmetryMatrix( fractionalPositions, glideReflection(fractionalPositions, Vector3d(0,0,0.5), Vector3d(0,0.5,0)) );
+    gammaNMatrix = getSymmetryMatrix( fractionalPositions, glideReflection(fractionalPositions, Vector3d(0.5,0.5,0), Vector3d(0,0,0.25))) ;
+
+    for( auto alpha : getPoweredMatrices(alphaBMatrix, maxN) ){
+        for( auto beta : getPoweredMatrices(betaCMatrix, maxN) ){
+            for( auto gamma : getPoweredMatrices(gammaNMatrix, maxN) ){
+                symmetriMatrices.push_back( alpha * beta * gamma );
+            }
+        }
+    }
+}
