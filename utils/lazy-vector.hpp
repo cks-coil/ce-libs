ef966e6dd8521ecd21634574ddb280984881f2bb
Chikashi Shinagawa <cks.shina@gmail.com>
2016-01-07 21:34:33 +0900

    lazy_vector: change for const

--- utils/lazy-vector.hpp
+++ utils/lazy-vector.hpp
@@ -2,15 +2,18 @@
 #include <functional>
 #include <iterator>
 
+
 #ifndef __INCLUDED_LAZY_VECTOR_HPP__
 #define __INCLUDED_LAZY_VECTOR_HPP__
 
 template <typename _Tp> class LazyVectorIterator;
+template <typename _Tp> class LazyVectorConstIterator;
 
 template <typename _Tp>
 class LazyVector: private std::vector< std::function<_Tp(void)> >{
 public:
     typedef LazyVectorIterator<_Tp> iterator;
+    typedef LazyVectorIterator<_Tp> const_iterator;
     using std::vector< std::function<_Tp(void)> >::assign;
     using std::vector< std::function<_Tp(void)> >::capacity;
     using std::vector< std::function<_Tp(void)> >::clear;
@@ -22,12 +25,14 @@ public:
     using std::vector< std::function<_Tp(void)> >::reserve;
     using std::vector< std::function<_Tp(void)> >::resize;
     using std::vector< std::function<_Tp(void)> >::size;
-    _Tp at(int pos){ return (std::vector< std::function<_Tp(void)> >::at(pos))(); }
-    _Tp operator[](int pos){ return (std::vector< std::function<_Tp(void)> >::operator[](pos))(); }
-    _Tp front(void){ return (std::vector< std::function<_Tp(void)> >::front())(); }
-    _Tp back(void){ return (std::vector< std::function<_Tp(void)> >::back())(); }
-    iterator begin(void){ return iterator(this, 0); }
-    iterator end(void){ return iterator(this, this->size()); }
+    _Tp at(int pos) const{ return (std::vector< std::function<_Tp(void)> >::at(pos))(); }
+    _Tp operator[](int pos) const{ return (std::vector< std::function<_Tp(void)> >::operator[](pos))(); }
+    _Tp front(void) const{ return (std::vector< std::function<_Tp(void)> >::front())(); }
+    _Tp back(void) const{ return (std::vector< std::function<_Tp(void)> >::back())(); }
+    iterator begin(void) { return iterator(this, 0); }
+    iterator end(void) { return iterator(this, this->size()); }
+    const_iterator begin(void) const { return const_iterator(this, 0); }
+    const_iterator end(void) const { return const_iterator(this, this->size()); }
 };
 
 template <typename _Tp>
@@ -47,23 +52,23 @@ public:
         _index++;
         return result;
     }
-    _Tp operator*(){
+    _Tp operator*() const{
         return _lazyVector->at(_index);
     }
-    bool operator==(const LazyVectorIterator<_Tp>& iterator){
+    bool operator==(const LazyVectorIterator<_Tp>& iterator) const{
         return (iterator._index == _index && iterator._lazyVector == _lazyVector);
     }
-    bool operator!=(const LazyVectorIterator<_Tp>& iterator){
+    bool operator!=(const LazyVectorIterator<_Tp>& iterator) const{
         return (iterator._index != _index || iterator._lazyVector != _lazyVector);
     }
 
 private:
-    LazyVectorIterator(LazyVector<_Tp> *lazyVector, int index){
+    LazyVectorIterator(LazyVector<_Tp> const *lazyVector, int index){
         _index = index;
         _lazyVector = lazyVector;
     }
     size_t _index;
-    LazyVector<_Tp> *_lazyVector;
+    LazyVector<_Tp> const *_lazyVector;
 };
 
 #endif
